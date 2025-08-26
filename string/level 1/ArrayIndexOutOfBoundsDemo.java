67f82be7ae26679d0955aa385e3d586afdd78b1e
Yash Raj Srivastava <yashraj102723@gmail.com>
2025-08-26 14:34:44 +0530

    [yash] Refactor used try catch to solve it

--- string/level 1/ArrayIndexOutOfBoundsDemo.java
+++ string/level 1/ArrayIndexOutOfBoundsDemo.java
@@ -4,10 +4,13 @@ public class ArrayIndexOutOfBoundsDemo {
         Scanner sc = new Scanner(System.in);
         String[] names = new String[3];
         System.out.println("Enter 3 names:");
-        for(int i = 0; i < names.length; i++){
+        for (int i = 0; i < names.length; i++) {
             names[i] = sc.next();
         }
-        System.out.println("Accessing index 5: " + names[5]);
-        sc.close();
+        try {
+            System.out.println("Accessing index 5:" + names[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error:" + e.getMessage());
+        }
     }
 }
