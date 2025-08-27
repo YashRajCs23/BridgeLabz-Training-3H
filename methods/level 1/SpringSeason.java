b1b39a51c07e4c94ff22adc1fbbf0bed47aa95ad
Yash Raj Srivastava <yashraj102723@gmail.com>
2025-08-27 20:18:11 +0530

    [Yash] made method to find the spring season

--- methods/level 1/SpringSeason.java
+++ methods/level 1/SpringSeason.java
@@ -3,6 +3,7 @@ public class SpringSeason {
         if((month==3&&day>=20)||(month==4)||(month==5)||(month==6&&day<=20)) return true;
         return false;
     }
+    
     public static void main(String[] args){
         int month=Integer.parseInt(args[0]);
         int day=Integer.parseInt(args[1]);
