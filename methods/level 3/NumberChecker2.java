331d4b0c35b3ce9ba662e907411336adafbef34d
Yash Raj Srivastava <yashraj102723@gmail.com>
2025-08-27 20:43:46 +0530

    [Yash] made methods for sumDigits sumSquares Harshad frequencyDigits

--- methods/level 3/NumberChecker2.java
+++ methods/level 3/NumberChecker2.java
@@ -29,6 +29,7 @@ public class NumberChecker2 {
         for(int i:d) freq[i][1]++;
         for(int i=0;i<10;i++) if(freq[i][1]>0) System.out.println(freq[i][0]+": "+freq[i][1]);
     }
+
     public static void main(String[] args) {
         int num=21;
         int[] digits=storeDigits(num);
