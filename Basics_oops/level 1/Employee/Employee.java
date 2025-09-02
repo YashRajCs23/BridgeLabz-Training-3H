86a914162a12ec6f098486235f9570818ea45819
Yash Raj Srivastava <yashraj102723@gmail.com>
2025-09-02 17:32:11 +0530

    [yash] added employee ques class and main using oops

--- Basics_oops/level 1/Employee/Employee.java
+++ Basics_oops/level 1/Employee/Employee.java
@@ -3,7 +3,6 @@ public class Employee{
     String name;
     int id;
     double salary;
-
     public Employee(String name, int id, double salary) {
         this.name = name;
         this.id = id;
--- Basics_oops/level 1/Employee/EmployeeMain.java
+++ Basics_oops/level 1/Employee/EmployeeMain.java
@@ -3,5 +3,6 @@ public class EmployeeMain {
     public static void main(String[] args) {
         Employee emp1 = new Employee("rajesh", 101, 50000);
         emp1.displayDetails();
+        
     }
 }
