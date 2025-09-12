6d4760cb689d77cd162a5be7aec3a2e0db5f6b50
Yash Raj Srivastava <yashraj102723@gmail.com>
2025-09-12 11:08:02 +0530

    [yash] added employee management system problem

--- Inheritance/Introducton/com.employeeManagementSystem.gla/Employee.java
+++ Inheritance/Introducton/com.employeeManagementSystem.gla/Employee.java
@@ -3,7 +3,6 @@ public class Employee {
     String name;
     int id;
     double salary;
-
     Employee(String name, int id, double salary) {
         this.name = name;
         this.id = id;
--- Inheritance/Introducton/com.employeeManagementSystem.gla/EmployeeSystem.java
+++ Inheritance/Introducton/com.employeeManagementSystem.gla/EmployeeSystem.java
@@ -6,6 +6,7 @@ public class EmployeeSystem {
             new Developer("Bob", 102, 70000, "Java"),
             new Intern("Charlie", 103, 20000, 6)
         };
+        
         for (Employee e : employees) {
             e.displayDetails();
             System.out.println("-------------------");
