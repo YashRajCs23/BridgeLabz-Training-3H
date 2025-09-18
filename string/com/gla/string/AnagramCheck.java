9fdc320ae0bc087af8254c26183abade714118a0
Yash Raj Srivastava <yashraj102723@gmail.com>
2025-09-18 23:04:20 +0530

    [yash] added anagram problem

---
+++ string/com/gla/string/AnagramCheck.java
@@ -0,0 +1,15 @@
+package com.gla.string;
+
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+        String s1 = "listen";
+        String s2 = "silent";
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        System.out.println(Arrays.equals(a, b) ? "Anagrams" : "Not Anagrams");
+    }
+}
