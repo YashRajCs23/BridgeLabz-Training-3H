3c9b49c6c10c1bfd43c3828470c157d5dc8e04b5
Yash Raj Srivastava <yashraj102723@gmail.com>
2025-09-18 23:08:59 +0530

    [yash] added palindrome strings problem

---
+++ string/com/gla/string/PalindromeCheck.java
@@ -0,0 +1,10 @@
+package com.gla.string;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String str = "madam";
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) rev += str.charAt(i);
+        System.out.println(str.equals(rev) ? "Palindrome" : "Not Palindrome");
+    }
+}
