24650dbfcf0f0245021782aa7fd289fb00f4ff4e
Yash Raj Srivastava <yashraj102723@gmail.com>
2025-09-18 23:10:53 +0530

    [yash] added replace word strings problem

---
+++ string/com/gla/string/ReplaceWord.java
@@ -0,0 +1,9 @@
+package com.gla.string;
+
+public class ReplaceWord {
+    public static void main(String[] args) {
+        String sentence = "I like Java and Java is powerful";
+        String result = sentence.replace("Java", "Python");
+        System.out.println(result);
+    }
+}
