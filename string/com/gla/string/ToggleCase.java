8ff84a3685ac8830949ed3a0e0dedd8bdd8c8592
Yash Raj Srivastava <yashraj102723@gmail.com>
2025-09-18 23:12:32 +0530

    [yash] added toggle case strings problem

---
+++ string/com/gla/string/ToggleCase.java
@@ -0,0 +1,14 @@
+package com.gla.string;
+
+public class ToggleCase {
+    public static void main(String[] args) {
+        String str = "HeLLo WoRld";
+        StringBuilder sb = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+            if (Character.isUpperCase(ch)) sb.append(Character.toLowerCase(ch));
+            else if (Character.isLowerCase(ch)) sb.append(Character.toUpperCase(ch));
+            else sb.append(ch);
+        }
+        System.out.println("Toggled: " + sb);
+    }
+}
